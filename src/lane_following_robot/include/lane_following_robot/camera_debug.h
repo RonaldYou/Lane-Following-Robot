fce77f9f57615f0bdb55f537b7cbf62335961144
RonaldYou <ronaldrenyou@gmail.com>
2025-09-20 23:36:49 +0200

    succesful build for camera_debug

--- src/lane_following_robot/include/lane_following_robot/camera_debug.h
+++ src/lane_following_robot/include/lane_following_robot/camera_debug.h
@@ -5,15 +5,17 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
-#include <image_transport/image_transport.hpp>
 
 class CameraDebug : public rclcpp::Node{
     private:
-        void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
-        image_transport::Subscriber image_sub_;
+        void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr& msg);
+
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr debug_image_pub_;
 
         int image_count_;
     public: 
         CameraDebug();
-}
+};
+
+#endif // CAMERA_DEBUG_HPP_
--- src/lane_following_robot/src/camera_debug.cpp
+++ src/lane_following_robot/src/camera_debug.cpp
@@ -1,13 +1,12 @@
 #include "lane_following_robot/camera_debug.h"
 
 CameraDebug::CameraDebug(): Node("camera_debug"), image_count_(0){
-    image_transport::ImageTransport it(this);
-    image_sub_ = it.subscribe("/camera/image_raw", 1, std::bind(&CameraDebug::image_callback, this, std::placeholders::_1));
+    image_sub_ = this->create_subscription<sensor_msgs::msg::Image>("/camera/image_raw", 10, std::bind(&CameraDebug::image_callback, this, std::placeholders::_1));
     debug_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/debug_image", 10);
     RCLCPP_INFO(this->get_logger(), "Camera Debug Node Initialized");
 }
 
-void CameraDebug::image_callback(const sensor_msgs::msg::Image::SharedPtr msg){
+void CameraDebug::image_callback(const sensor_msgs::msg::Image::ConstSharedPtr& msg){
     cv_bridge::CvImagePtr cv_ptr;
     try{
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -17,12 +16,25 @@ void CameraDebug::image_callback(const sensor_msgs::msg::Image::SharedPtr msg){
 
         std::string frame_text = "Frame: " + std::to_string(image_count_);
         cv::putText(debug_image, frame_text, cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
+
+        sensor_msgs::msg::Image::SharedPtr debug_msg = cv_bridge::CvImage(msg->header, "bgr8", debug_image).toImageMsg();
+        debug_image_pub_->publish(*debug_msg);
     } catch (cv_bridge::Exception& e) {
         RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
         return;
     }
 
-    sensor_msgs::msg::Image::SharedPtr debug_msg = cv_bridge::CvImage(msg->header, "bgr8", debug_image).toImageMsg();
-    debug_image_pub_->publish(*debug_msg);
+}
 
+int main(int argc, char** argv){
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CameraDebug>();
+    
+    RCLCPP_INFO(node->get_logger(), "Starting camera debug node...");
+    
+    rclcpp::spin(node);
+    
+    RCLCPP_INFO(node->get_logger(), "Camera debug node shutting down.");
+    rclcpp::shutdown();
+    return 0;
 }
